6a4948b393b2a535f64d7c8e0fa1ac6708b37a5a
sindhura <sindhura343434@gmail.com>
2023-04-26 18:29:05 +0530

    Method Overloading

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args){
+        Product p=new Product();
+        p.product(2,5);
+        p.product(2, 6, 7);
+        p.product(22.05,55.07);
+    }
+public static class Product{
+        public static int product(int x,int y){
+            return x*y;
+        }
+        public  static int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public static  double product(double x,double y){
+            return x*y;
+        }
+}
 }
